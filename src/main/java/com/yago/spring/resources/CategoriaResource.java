fd5e41262d9ae8c49afbf7fc39135c5d40de327c
Yago Rodrigo <yagorodrigo9@gmail.com>
2020-04-22 15:27:46 -0300

    Testando a primeira classe de dominio

--- src/main/java/com/yago/spring/resources/CategoriaResource.java
+++ src/main/java/com/yago/spring/resources/CategoriaResource.java
@@ -1,16 +1,29 @@
 package com.yago.spring.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.yago.spring.domain.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 	
 	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {
-		return "rest esta funcionando";
+	public List<Categoria> listar() {
+		
+		Categoria cat1 = new Categoria(1, "Informatica");
+		Categoria cat2 = new Categoria(2, "Escritorio");
+		
+		List<Categoria> lista = new ArrayList<>();
+		lista.add(cat1);
+		lista.add(cat2);
+		
+		return lista;
 	}
 
 }
