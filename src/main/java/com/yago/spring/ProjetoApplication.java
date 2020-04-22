f199f9d545f46c40f60884313dad6506f6aec964
Yago Rodrigo <yagorodrigo9@gmail.com>
2020-04-22 17:08:15 -0300

    Criando eperação de instanciação

--- src/main/java/com/yago/spring/ProjetoApplication.java
+++ src/main/java/com/yago/spring/ProjetoApplication.java
@@ -1,13 +1,32 @@
 package com.yago.spring;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class ProjetoApplication {
+import com.yago.spring.domain.Categoria;
+import com.yago.spring.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class ProjetoApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(ProjetoApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritorio");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 	}
 
 }
